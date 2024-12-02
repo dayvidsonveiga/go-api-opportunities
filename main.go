e1e645c18560a4d9c9dd4b0492219939b01cc93b
Dayvidson Luis Veiga Gomes da Silva <dayvidsonveiga@gmail.com>
2024-12-02 00:39:02 -0300

    fead: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import "github.com/dayvidsonveiga/go-api-opportunities/router"
+import (
+	"github.com/dayvidsonveiga/go-api-opportunities/config"
+	"github.com/dayvidsonveiga/go-api-opportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
-	// Initialize Router
+	logger = config.GetLogger("main")
+	// Initialize Config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Error initializing config: %v", err)
+		return
+	}
 
+	// Initialize Router
 	router.Initialize()
 }
